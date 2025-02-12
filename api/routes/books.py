a8c8b96f5b18b7bbaa2d8b166926313eb510780f
K Odedele <odedeleg@gmail.com>
2025-02-12 10:35:33 -0600

    fix: test test

--- api/routes/books.py
+++ api/routes/books.py
@@ -65,5 +65,5 @@ async def delete_book(book_id: int) -> None:
 async def get_book(book_id: int) -> Book:
     book = db.get_book(book_id)
     if book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
+        raise HTTPException(status_code=404, detail="Book not found, this is a test")
     return book
